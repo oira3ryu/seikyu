064430498393b95850f3d772636d653bf493790c
yukih <yukih@DESKTOP-O7J1KPA>
2021-01-11 21:04:03 +0900

    2021011102

--- src/main/java/com/example/demo/CsvController.java
+++ src/main/java/com/example/demo/CsvController.java
@@ -3,80 +3,58 @@ package com.example.demo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.demo.model.Csv;
 import com.example.demo.service.CsvService;
 
-@CrossOrigin("http://localhost:8080")
 @Controller
-@RequestMapping("/api/csv")
+@RequestMapping("/Csv")
 public class CsvController {
+	@Autowired
+	private CsvService CsvService;
 
-  @Autowired
-  CsvService fileService;
+	@GetMapping
+	  public String csvIndex(Model model) {
+	    List<Csv> csvs = CsvService.findAll();
+	    model.addAttribute("csvs", csvs);
+	    return "csvIndex";
+	}
 
-  @PostMapping("/upload")
-  public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
-    String message = "";
+	@GetMapping("csvNew")
+	public String newCsv(Model model) {
+		return "csvNew";
+	}
 
-    if (CsvHelper.hasCSVFormat(file)) {
-      try {
-        fileService.save(file);
+	@GetMapping("{id}/edit")
+	public String edit(@PathVariable Integer id,Model model) {
+		Csv csv = CsvService.findOne(id);
+		model.addAttribute("csv",csv);
+		return "csvEdit";
+	}
 
-        message = "Uploaded the file successfully: " + file.getOriginalFilename();
+	@PostMapping
+	public String create(@ModelAttribute Csv csv) {
+		CsvService.save(csv);
+		return "redirect:/Csv";
+	}
 
-        String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
-                .path("/api/csv/download/")
-                .path(file.getOriginalFilename())
-                .toUriString();
-
-        return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message,fileDownloadUri));
-      } catch (Exception e) {
-        message = "Could not upload the file: " + file.getOriginalFilename() + "!";
-        return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message,""));
-      }
+	@PostMapping("{id}/edit")
+    public String update(@PathVariable Integer id, @ModelAttribute Csv csv) {
+        csv.setId(id);
+        CsvService.save(csv);
+        return "redirect:/Csv";
     }
 
-    message = "Please upload a csv file!";
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(message,""));
-  }
-
-  @GetMapping("/tutorials")
-  public ResponseEntity<List<DeveloperTutorial>> getAllTutorials() {
-    try {
-      List<DeveloperTutorial> tutorials = fileService.getAllTutorials();
-
-      if (tutorials.isEmpty()) {
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-      }
-
-      return new ResponseEntity<>(tutorials, HttpStatus.OK);
-    } catch (Exception e) {
-      return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+    @PostMapping("{id}")
+    public String destroy(@PathVariable Integer id) {
+        CsvService.delete(id);
+        return "redirect:/Csv";
     }
-  }
-
-  @GetMapping("/download/{fileName:.+}")
-  public ResponseEntity<Resource> downloadFile(@PathVariable String fileName) {
-    InputStreamResource file = new InputStreamResource(fileService.load());
-
-    return ResponseEntity.ok()
-        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName)
-        .contentType(MediaType.parseMediaType("application/csv"))
-        .body(file);
-  }
 }
--- src/main/java/com/example/demo/model/Csv.java
+++ src/main/java/com/example/demo/model/Csv.java
@@ -9,6 +9,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,14 +23,85 @@ import lombok.Setter;
 @AllArgsConstructor
 @Getter
 @Setter
+@JsonPropertyOrder({
+	"id"
+	, "c1"
+	, "c2"
+	, "c3"
+	, "c4"
+	, "c5"
+	, "c6"
+	, "c7"
+	, "c8"
+	, "c9"
+	, "c10"
+	, "c11"
+	, "c12"
+	, "c13"
+	, "c14"
+	, "c15"
+	, "c16"
+	, "c17"
+	, "c18"
+	, "c19"
+	, "c20"
+	, "c21"
+	, "c22"
+	, "c23"
+	, "c24"
+	, "c25"
+	, "c26"
+	, "c27"
+	, "c28"
+	, "c29"
+	, "c30"
+	, "c31"
+	, "c32"
+	, "c33"
+	, "c34"
+	, "c35"
+	, "c36"
+	, "c37"
+	, "c38"
+	, "c39"
+	, "c40"
+	, "c41"
+	, "c42"
+	, "c43"
+	, "c44"
+	, "c45"
+	, "c46"
+	, "c47"
+	, "c48"
+	, "c49"
+	, "c50"
+	, "c51"
+	, "c52"
+	, "c53"
+	, "c54"
+	, "c55"
+	, "c56"
+	, "c57"
+	, "c58"
+	, "time_stamp"
+	, "s_id"
+	, "p_id"
+	, "req_id"
+	, "nen"
+	, "tsuki"
+	, "g_id"
+	, "o_id"
+})
 
 public class Csv {
     @Id
     @Column(name="id")
     @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @JsonProperty("id")
     private Integer id;
 
     @Column(name="c1")
+    @JsonProperty("c1")
     private String c1;
 
     @Column(name="c2")
